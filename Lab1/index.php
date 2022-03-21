616a3c6e95ebed3272f63779226b7306f67420a0
HaderMostafa <hadeermostafa.094@gmail.com>
2022-03-21 14:26:40 +0200

    modification

--- Lab1/index.php
+++ Lab1/index.php
@@ -20,6 +20,22 @@ if (isset($_POST["submit"])) {
     $data = json_decode($result, true);
 
     //recommended method
-    highlight_string('<?php ' . var_export($data, true) . ';?>');
+    /*  // //highlight_string('<?php ' . var_export($data, true) . ';?>');*/
 
+$City_name = $data["name"];
+$cloud_desc = $data["weather"][0]["main"] . ": " . $data["weather"][0]["description"];
+$Temp = "Temprsure: " . $data["main"]["temp_min"] . " C " . $data["main"]["temp_max"] . " C";
+$humaditiy = "Humidity: " . $data["main"]["humidity"] . " %";
+$wind = "Wind: " . $data["wind"]["speed"] . " km/h";
+
+echo "<center>" . $City_name . "</center>";
+echo "</br>
+<center>" . $cloud_desc . "</center>";
+echo "</br>
+<center>" . $Temp . "</center>";
+echo "</br>
+<center>" . $humaditiy . "</center>";
+echo "</br>
+<center>" . $wind . "</center>";
+die();
 }
